7644929e7bf71a0ee124382436af44248eeb8fc5
JoaoPedroourique <35915066+JoaoPedroourique@users.noreply.github.com>
2019-11-29 19:18:50 -0300

    Add files via upload

---
+++ tela_jogar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "raylib.h"
+#include "globais.h"
+
+void atualizajogo(){
+    
+}
+
+void desenhajogo(){
+    BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+        DrawCircleV(posicaojogador, 50, MAROON);
+        
+     EndDrawing();
+        //----------------------------------------------------------------------------------
+    
+}
